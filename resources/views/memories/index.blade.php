f507edaa769dd38a7420fbdad0c81c568c8037e4
ttumasaki <be_da_masa@ybb.ne.jp>
2021-02-25 00:06:08 +0900

    不要な箇所を削除

--- resources/views/memories/index.blade.php
+++ resources/views/memories/index.blade.php
@@ -40,32 +40,3 @@
     </div>
 </div>
 @endsection
-<!-- 
-
-
-<h1>波情報</h1>
-
-<h2>皆の投稿</h2>
-
-@foreach($values as $value)
-確認ポイント
-{{$value->point}}<br>
-確認日時
-{{$value->date}}<br>
-波のサイズ
-{{$value->size}}<br>
-面の状況
-{{$value->w_condition}}<br>
-波数
-{{$value->number}}<br>
-波の状態
-{{$value->state}}<br>
-風の向き
-{{$value->direction}}<br>
-人数
-{{$value->people}}<br>
-水温
-{{$value->temperature}}<br>
-波の画像・動画
-{{$value->image}}<br>
-@endforeach -->
