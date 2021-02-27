da0a36dd6e538bf4df570d2c0ac4acae97d93de3
ttumasaki <be_da_masa@ybb.ne.jp>
2021-02-27 21:41:23 +0900

    投稿画面の作成、一覧表示の設定

--- resources/views/memories/index.blade.php
+++ resources/views/memories/index.blade.php
@@ -2,42 +2,44 @@
 
 @section('content')
 <div class="container">
+    <form method='GET' action="{{route('memories.create')}}">
+        <button type='submit' class='btn btn-primary'>波情報を登録</button>
+    </form>
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-              @foreach($values as $value)
-                <div class="card-header">確認ポイント:{{$value->point}}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-
-                    確認日時
-                    {{$value->date}}<br>
-                    波のサイズ
-                    {{$value->size}}<br>
-                    面の状況
-                    {{$value->w_condition}}<br>
-                    波数
-                    {{$value->number}}<br>
-                    波の状態
-                    {{$value->state}}<br>
-                    風の向き
-                    {{$value->direction}}<br>
-                    人数
-                    {{$value->people}}<br>
-                    水温
-                    {{$value->temperature}}<br>
-                    波の画像・動画
-                    {{$value->image}}<br>
-                    @endforeach
+        <ul class="list-group">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
+            @endif
+            @foreach($memories as $memory)
+                <li class="list-group-item active" aria-current="true">確認ポイント : {{$memory->point}}</li>
+                <li class="list-group-item">確認日時 : {{$memory->date}}</li>
+                <li class="list-group-item">波のサイズ : {{$size}}</li>
+                <li class="list-group-item">面の状況 : {{$w_condition}}</li>
+                <li class="list-group-item">波数 : {{$number}}</li>
+                <li class="list-group-item">波の状態 : {{$state}}</li>
+                <li class="list-group-item">風の向き : {{$direction}}</li>
+                <li class="list-group-item">人数 : {{$memory->people}}</li>
+                <li class="list-group-item">波の画像・動画 : {{$memory->image}}</li>
+                <li class="list-group-item">
+                </li>
+            @endforeach
+</ul>
+
+
+
+            <!-- <div class="card">
+
+                <div class="card-header"></div>
+
+                <div class="card-body"> -->
+
+                    
+                <!-- </div>
             </div>
-        </div>
+        </div> -->
     </div>
 </div>
 @endsection
